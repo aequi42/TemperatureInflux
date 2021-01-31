d95350c90480b5dc89d87e041499759eddcd266b
aequi42 <aequi42@users.noreply.github.com>
2021-01-31 01:39:54 +0100

    Make output configurable

--- README.md
+++ README.md
@@ -16,9 +16,17 @@ Download the Repository, build and add the following into your `telegraf.conf`:
   #name_suffix = "_mycollector"
 ```
 
+## Configure the output
 
-## Future Plans
+To configure the output, simply update the values inside the `TemperatureInflux.exe.config`.
 
-Make the output more configurable... currently everything is hard coded for my needs.
+`ModuleName`: The name of the module for influx  
+
+`EnabledModules`: Comma seperated list of modules which should be loaded (impacts performance). 
+Allowed values are: `CPU`, `RAM`, `Mobo`, `GPU`, `HDD`, `Fan`
+
+`Sensors`: Comma seperated list of sensors to output. To get all available sensors, run `.\TemperatureInflux.exe list`
+
+
 
 This Tool started by using the Code from http://www.lattepanda.com/topic-f11t3004.html
--- TemperatureInflux/Program.cs
+++ TemperatureInflux/Program.cs
@@ -1,6 +1,7 @@
 using OpenHardwareMonitor.Hardware;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 
 namespace TemperatureInflux
@@ -24,45 +25,95 @@ namespace TemperatureInflux
 
         private static void WriteMetric(string instance, IEnumerable<Tuple<string, string>> fields)
         {
+            var moduleName = ConfigurationManager.AppSettings["ModuleName"];
             var tags = $"host={System.Net.Dns.GetHostEntry("").HostName},instance={instance.Replace(" ", "\\ ")}";
             var field = string.Join(",", fields.Select(f => $"{f.Item1.Replace(" ", "\\ ")}={f.Item2.Replace(" ", "\\ ")}"));
             var timestamp = DateTimeOffset.Now.ToUnixTimeMilliseconds() * 1000000;
-            Console.WriteLine($"win_temp,{tags} {field} {timestamp}");
+            Console.WriteLine($"{moduleName},{tags} {field} {timestamp}");
         }
 
-        private static void GetSystemInfo()
+        private static void GetConfiguredSystemInfo()
+        {
+            var updateVisitor = new UpdateVisitor();
+            var computer = new Computer();
+            computer.Open();
+            setModules(computer);
+            computer.Accept(updateVisitor);
+            var sensors = GetAllSensors(computer.Hardware);
+            var filtered = FilterSensors(sensors);
+            var groupedByHW = filtered.GroupBy(s => s.Hardware.Name);
+            foreach (var hwGroup in groupedByHW)
+            {
+                var values = hwGroup
+                        .Select(s => Tuple.Create(s.Name, s.Value?.ToString(System.Globalization.CultureInfo.InvariantCulture) ?? String.Empty));
+                WriteMetric(hwGroup.Key, values);
+            }
+
+            computer.Close();
+        }
+
+        private static IEnumerable<IHardware> FlattenHardware(IHardware hardware)
+        {
+            var subHardware = hardware.SubHardware.SelectMany(s => FlattenHardware(s));
+            return new[] { hardware }.Union(subHardware);
+        }
+
+        private static IEnumerable<ISensor> GetAllSensors(IEnumerable<IHardware> hardware)
+        {
+            var allHardware = hardware.SelectMany(h => FlattenHardware(h));
+            return allHardware.SelectMany(h => h.Sensors);
+        }
+
+        private static Computer setModules(Computer computer)
+        {
+            var settings = ConfigurationManager.AppSettings["EnabledModules"];
+            var modules = settings.Split(',').Select(s => s.ToLower());
+            computer.CPUEnabled = modules.Contains("cpu");
+            computer.MainboardEnabled = modules.Contains("mobo");
+            computer.RAMEnabled = modules.Contains("ram");
+            computer.HDDEnabled = modules.Contains("hdd");
+            computer.FanControllerEnabled = modules.Contains("fan");
+            computer.GPUEnabled = modules.Contains("gpu");
+            return computer;
+        }
+
+        private static IEnumerable<ISensor> FilterSensors(IEnumerable<ISensor> sensors)
+        {
+
+            var settings = ConfigurationManager.AppSettings["Sensors"];
+            var sensorsToShow = settings.Split(',');
+            return sensors.Where(s => sensorsToShow.Contains(s.Identifier.ToString()));
+        }
+
+        private static void ListOptions()
         {
             var updateVisitor = new UpdateVisitor();
             var computer = new Computer();
             computer.Open();
             computer.CPUEnabled = true;
             computer.MainboardEnabled = true;
+            computer.RAMEnabled = true;
+            computer.HDDEnabled = true;
+            computer.FanControllerEnabled = true;
+            computer.GPUEnabled = true;
             computer.Accept(updateVisitor);
-            for (int i = 0; i < computer.Hardware.Length; i++)
+            var sensors = GetAllSensors(computer.Hardware);
+            Console.WriteLine("Identifier                    Name                Value     ");
+            Console.WriteLine("----------------------------- ------------------- ----------");
+            foreach (var sensor in sensors)
             {
-                if (computer.Hardware[i].HardwareType == HardwareType.CPU)
-                {
-                    var cpuTemps = computer.Hardware[i].Sensors
-                        .Where(s => s.SensorType == SensorType.Temperature)
-                        .Select(s => Tuple.Create(s.Name, s.Value.Value.ToString(System.Globalization.CultureInfo.InvariantCulture)));
-                    WriteMetric("CPU Temp", cpuTemps);
-
-                }
-                if (computer.Hardware[i].HardwareType == HardwareType.Mainboard)
-                {
-                    var temps = computer.Hardware[i].SubHardware[0].Sensors.Where(s => s.SensorType == SensorType.Temperature)
-                        .Select(s => Tuple.Create(s.Name, s.Value.Value.ToString(System.Globalization.CultureInfo.InvariantCulture)));
-                    WriteMetric("Mobo Temp", temps);
-                }
+                Console.WriteLine($"{sensor.Identifier,-30} {sensor.Name,-20 } {sensor.Value,-10}");
             }
-            computer.Close();
         }
 
         private static void Main(string[] args)
         {
-
-            GetSystemInfo();
+            if (args.Length > 0 && args[0] == "list")
+                ListOptions();
+            else
+                GetConfiguredSystemInfo();
 #if DEBUG
+            Console.WriteLine($"Press enter to exit...");
             Console.ReadLine();
 #endif
         }
